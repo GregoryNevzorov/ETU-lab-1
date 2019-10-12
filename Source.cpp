18ccc541694d0dfa05a5cf0c2524f97019d6c4d3
GregoryNevzorov <gregoryofrivia@gmail.com>
2019-10-12 18:20:59 +0300

    br_short_int() add

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <Windows.h> //Дает возможность изменять цвет выводимых в консоль символов.
 
 using namespace std;
 
@@ -7,8 +8,10 @@ void br_int()
 	cout << "Enter an integer. (-2147483648 <= x <= 2147483647)\n";
 	int integer_number;
 	cin >> integer_number;
-	//Значение для сравнения с битами вводимого числа. проверка
+	//Значение для сравнения с битами вводимого числа.
 	int marker = 1 << 31;
+	//Для отображения значащих разрядов.
+	short int significant_rank = 0;
 	//Двигаемся от страшего (знакового) бита к младшему.
 	for (short int i = 0; i < 32; i++)
 	{
@@ -21,6 +24,13 @@ void br_int()
 			}
 			else
 			{
+				if (significant_rank == 0)
+				{
+					significant_rank += 1;
+					//Выделение значащих разрядов цветом.
+					HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+                    SetConsoleTextAttribute(handle, FOREGROUND_GREEN);
+				}
 				cout << "1";
 				marker = marker >> 1;
 			}
@@ -40,6 +50,61 @@ void br_int()
 		}
 	}
 	cout << endl;
+	//Обратная процедура
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // = DARKGRAY
+}
+
+void br_short_int()
+{
+	cout << "Enter an short integer. (-32768 <= x <= 32767)\n";
+	int short_integer_number;
+	cin >> short_integer_number;
+	//Значение для сравнения с битами вводимого числа.
+	int marker = 1 << 15;
+	//Для отображения значащих разрядов.
+	short int significant_rank = 0;
+	//Двигаемся от страшего (знакового) бита к младшему.
+	for (short int i = 0; i < 16; i++)
+	{
+		if ((marker & short_integer_number) == marker)
+		{
+			if (i == 0)
+			{
+				cout << "1 ";
+				marker = 1 << 14;
+			}
+			else
+			{
+				if (significant_rank == 0)
+				{
+					significant_rank += 1;
+					//Выделение значащих разрядов цветом.
+					HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+					SetConsoleTextAttribute(handle, FOREGROUND_GREEN);
+				}
+				cout << "1";
+				marker = marker >> 1;
+			}
+		}
+		else
+		{
+			if (i == 0)
+			{
+				cout << "0 ";
+				marker = 1 << 14;
+			}
+			else
+			{
+				cout << "0";
+				marker = marker >> 1;
+			}
+		}
+	}
+	cout << endl;
+	//Обратная процедура
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // = DARKGRAY
 }
 
 int main()
@@ -58,7 +123,7 @@ int main()
 		case 1:
 			br_int();
 		case 2:
-			;
+			br_short_int();
 		case 3:
 			;
 		case 4:
